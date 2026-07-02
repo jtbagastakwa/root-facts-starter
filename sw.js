6717603b5abce40882c71574b8ac55403f0ff7e3
jtbagastakwa <bagastakwa0411@gmail.com>
2026-07-02 16:21:37 +0700

    Bump sw.js revision to force cache update

--- sw.js
+++ sw.js
@@ -5,21 +5,21 @@ if (workbox) {
   
   // Set precache and route
   workbox.precaching.precacheAndRoute([
-    { url: '/index.html', revision: '1' },
-    { url: '/manifest.json', revision: '1' },
-    { url: '/assets/css/styles.css', revision: '1' },
-    { url: '/assets/js/core/app.js', revision: '1' },
-    { url: '/assets/js/core/config.js', revision: '1' },
-    { url: '/assets/js/core/utils.js', revision: '1' },
-    { url: '/assets/js/services/camera.service.js', revision: '1' },
-    { url: '/assets/js/services/detection.service.js', revision: '1' },
-    { url: '/assets/js/services/facts.service.js', revision: '1' },
-    { url: '/assets/js/ui/ui.handler.js', revision: '1' },
-    { url: '/model/model.json', revision: '1' },
-    { url: '/model/metadata.json', revision: '1' },
-    { url: '/model/weights.bin', revision: '1' },
-    { url: '/assets/icons/icon-192x192.png', revision: '1' },
-    { url: '/assets/icons/apple-touch-icon.png', revision: '1' }
+    { url: '/index.html', revision: '2' },
+    { url: '/manifest.json', revision: '2' },
+    { url: '/assets/css/styles.css', revision: '2' },
+    { url: '/assets/js/core/app.js', revision: '2' },
+    { url: '/assets/js/core/config.js', revision: '2' },
+    { url: '/assets/js/core/utils.js', revision: '2' },
+    { url: '/assets/js/services/camera.service.js', revision: '2' },
+    { url: '/assets/js/services/detection.service.js', revision: '2' },
+    { url: '/assets/js/services/facts.service.js', revision: '2' },
+    { url: '/assets/js/ui/ui.handler.js', revision: '2' },
+    { url: '/model/model.json', revision: '2' },
+    { url: '/model/metadata.json', revision: '2' },
+    { url: '/model/weights.bin', revision: '2' },
+    { url: '/assets/icons/icon-192x192.png', revision: '2' },
+    { url: '/assets/icons/apple-touch-icon.png', revision: '2' }
   ]);
 
   // Route for model bin files to be cached safely
